5e2240c21b2b943f56f2aec0a099f3eb004a20e4
mizuno <it212258@trident.ac.jp>
2023-12-18 14:57:37 +0900

    Sighin　微調整

--- T-CHAT-Signin/T-CHAT-Signin.js
+++ T-CHAT-Signin/T-CHAT-Signin.js
@@ -9,44 +9,20 @@ var firebaseConfig = {
     };
 
 firebase.initializeApp(firebaseConfig);
-var auth = firebase.auth();
-var db   = firebase.firestore();
+const firestore = firebase.firestore();
+const user_register = firestore.collection('UserRegister');
+const username = document.getElementById("username");
+const password = document.getElementById("password");
+const mailaddress = document.getElementById("mailaddress");
+const register_button = document.getElementById("register_button");
 
-function login(email , password){
-    auth.signInWithEmailAndPassword(email,password)
-    .then((userCredential) => {
-        var user = userCredential.user;
-        return db.collection("users").doc(user,id).get();
-    })
-    .then((doc) => {
-        var userData = doc.data();
-        var username = userData.username;
-    })
-    .catch((error) => {
-        // ログイン失敗時の処理
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        console.error(errorMessage);
+register_button.addEventListener("click", async function(){
+    if(username.value !="" && password.value != "" && mailaddress.value != ""){
+        
+    await user_register.add({
+        UserName: username.value,
+        PassWord: password.value,
+        MailAddress: mailaddress.value
     });
-}
-document.getElementById("SigninForm").addEventListener("submit", function(event) {
-event.preventDefault();
-
-    // const username = document.getElementById("username").value;
-    // const password = document.getElementById("password").value;
-    // const mailaddress = document.getElementById("mailaddress")
-
-    // firebase
+    };
 });
-
-    // // ダミーのユーザー情報
-    // const dummyUsername = "user";
-    // const dummyPassword = "password";
-
-    // if (username === dummyUsername && password === dummyPassword) {
-    //     // ログイン成功時の処理
-    //     document.getElementById("error-message").textContent = "Login successful!";
-    // } else {
-    //     // ログイン失敗時の処理
-    //     document.getElementById("error-message").textContent = "Invalid username or password";
-    // }
