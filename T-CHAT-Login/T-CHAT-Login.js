cdcb2cb38fde6f8d83a3d14214c975df01cd4b95
mizuno <it212258@trident.ac.jp>
2023-12-19 12:39:24 +0900

    Login 機能完成

--- T-CHAT-Login/T-CHAT-Login.js
+++ T-CHAT-Login/T-CHAT-Login.js
@@ -1,18 +1,46 @@
-document.getElementById("loginForm").addEventListener("submit", function(event) {
-    event.preventDefault();
+var firebaseConfig = {
+    apiKey: "AIzaSyARxI5dZXILhMkMDTDE5MyK88yJlCh-A_Y",
+    authDomain: "t-chat-d4c62.firebaseapp.com",
+    projectId: "t-chat-d4c62",
+    storageBucket: "t-chat-d4c62.appspot.com",
+    messagingSenderId: "276479107458",
+    appId: "1:276479107458:web:329742b4d052a975d16f9b",
+    measurementId: "G-WPZGDY4H0F"
+    };
 
-    // ダミーのユーザー情報
-    const dummyUsername = "user";
-    const dummyPassword = "password";
+firebase.initializeApp(firebaseConfig);
+const firestore = firebase.firestore();
+const user_register = firestore.collection('UserRegister');
+const username_or_mail = document.getElementById("username_or_mail");
+const password = document.getElementById("password");
+const register_button = document.getElementById("register_button");
+const error_message = document.getElementById("error-message");
 
-    const username = document.getElementById("username").value;
-    const password = document.getElementById("password").value;
+register_button.addEventListener("click", async function(){
+    const input_UserInfo = username_or_mail.value;
+    const input_PassWord = password.value;
+    // ユーザー情報の重複をチェック
+    const querySnapshot = await user_register
+    .get();
+    let isDuplicate = false;
 
-    if (username === dummyUsername && password === dummyPassword) {
-        // ログイン成功時の処理
-        document.getElementById("error-message").textContent = "Login successful!";
-    } else {
-        // ログイン失敗時の処理
-        document.getElementById("error-message").textContent = "Invalid username or password";
-    }
+    querySnapshot.forEach(doc => {
+      const user = doc.data();
+      if (user.UserName === input_UserInfo || user.MailAddress === input_UserInfo) {
+          isDuplicate = true;
+      }
+  });
+
+  if (username_or_mail.value !== "" && password.value !== "") {
+      if (isDuplicate) {
+          // 重複がある場合の処理
+          console.log("ログイン");
+        } else {
+          // 重複がない場合の処理
+          console.log("ログイン失敗");
+      }
+  } else {
+      // 入力が不足している場合の処理
+      console.log("入力が不足しています。失敗");
+  }
 });
