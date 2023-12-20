ff4af32a66f9f95b7ffd0a5831ed9b495b7bd941
mizuno <it212258@trident.ac.jp>
2023-12-20 16:32:17 +0900

    post.js 更新

--- T-CHAT-Post/T-CHAT-Post.js
+++ T-CHAT-Post/T-CHAT-Post.js
@@ -129,12 +129,7 @@ const editor1 = new Quill("#editor_area", {
 
   theme: "snow",
 });
-const format_content = document.getElementById("format");
-const title_content = document.getElementById("title");
-const post_button = document.getElementById("post");
-const question_tag_content = document.getElementById("question_tag");
-const textarea_content = document.getElementById("textarea");
-post_button.addEventListener("click", async function () {});
+
 // ここのツールバーはカスタムできます。
 var toolbarOptions = [
   [{ header: [2, 3, false] }],
@@ -157,4 +152,31 @@ const editor2 = new Quill("#editor_area", {
     "ここに質問したいことを記載してください\n例）\nFirebaseのデータベースを使用してログイン画面を作っています。\nその途中で問題が発生しました。その問題とはFirebaseの構成が理解出来ないんですよ～、\nなんで教えてほしいですね～。",
 
   theme: "snow",
+});
+
+var firebaseConfig = {
+  apiKey: "AIzaSyARxI5dZXILhMkMDTDE5MyK88yJlCh-A_Y",
+  authDomain: "t-chat-d4c62.firebaseapp.com",
+  projectId: "t-chat-d4c62",
+  storageBucket: "t-chat-d4c62.appspot.com",
+  messagingSenderId: "276479107458",
+  appId: "1:276479107458:web:329742b4d052a975d16f9b",
+  measurementId: "G-WPZGDY4H0F",
+};
+
+firebase.initializeApp(firebaseConfig);
+const firestore = firebase.firestore();
+const user_register = firestore.collection("UserRegister");
+const post_register = firestore.collection("Post");
+const format_content = document.getElementById("format");
+const title_content = document.getElementById("title");
+const post_button = document.getElementById("post");
+const question_tag_content = document.getElementById("question_tag");
+const textarea_content = document.getElementById("textarea");
+post_button.addEventListener("click", async function () {
+  const postContent = {
+    Title: title_content,
+    Content: textarea_content,
+    Format: format_content,
+  };
 });
